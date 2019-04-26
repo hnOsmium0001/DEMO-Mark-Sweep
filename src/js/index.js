caa4d93af4d5f9669f2d212387456baf9ac1c7a6
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-04-26 14:17:09 +0800

    Test utility, add "type=module" on script tag

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 import { VirtualObject } from './memory/objects';
 import { Heap } from './memory/heap';
+import { MarkSweep } from './memory/gc_algorithum';
 
-const heap = new Heap();
-const obj = new VirtualObject(0, 8, []);
+const heap = new Heap(64);
+heap.gc = new MarkSweep(heap);
+
+const obj = VirtualObject.create(2, [], heap);
+heap.addReference(obj);
+
+console.log(heap.references);
+console.log(heap.fragmentsOccupied.fragmentsStorage);
+console.log(heap.fragmentsFree.fragmentsStorage);
