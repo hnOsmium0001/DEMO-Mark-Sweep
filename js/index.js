1bbfd79ac76b70982ceba899d7ea4d091b4efc49
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-04-26 16:50:39 +0800

    Update GC test

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ import { MarkSweep } from './memory/gc_algorithum.js';
 const heap = new Heap(64);
 heap.gc = new MarkSweep(heap);
 
-const obj_o1_1 = VirtualObject.create(2, [obj_o3], heap); // 0
+const obj_o1_1 = VirtualObject.create(2, [], heap); // 0
 const obj_o1 = VirtualObject.create(3, [obj_o1_1], heap); // 1
 
-const obj_o2_1 = VirtualObject.create(2, [obj_o3, obj_o2_2], heap); // 2
+const obj_o2_1 = VirtualObject.create(2, [obj_o2_2], heap); // 2
 const obj_o2_2 = VirtualObject.create(2, [], heap); // 3
 const obj_o2 = VirtualObject.create(6, [obj_o2_1, obj_o2_2], heap); // 4
 
@@ -19,10 +19,12 @@ const obj2 = VirtualObject.create(4, [obj_o1, obj], heap); // 7
 heap.addReference(obj);
 heap.addReference(obj2);
 
-const randomObj1 = VirtualObject.create(2, [randomObj2], heap); // 8
+// Loop referencing
+const randomObj1 = VirtualObject.create(2, [], heap); // 8
 const randomObj2 = VirtualObject.create(8, [randomObj1, obj_o1, obj], heap) // 9
+randomObj1.ref.push(randomObj2);
 
-const selfObj = VirtualObject.create(1, [selfObj], heap) // 10
+const wildObj = VirtualObject.create(1, [], heap) // 10
 
 console.log("Order: objects, occupied, free")
 console.log(heap.objects);
