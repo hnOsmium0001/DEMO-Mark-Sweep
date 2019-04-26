df748334aea6e402e90c578484b6596de14e482f
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-04-26 17:14:23 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -9,13 +9,17 @@ const obj_o1_1 = VirtualObject.create(2, [], heap); // 0
 const obj_o1 = VirtualObject.create(3, [obj_o1_1], heap); // 1
 
 const obj_o2_1 = VirtualObject.create(2, [], heap); // 2
-const obj_o2_2 = VirtualObject.create(2, [obj_o2_2], heap); // 3
+const obj_o2_2 = VirtualObject.create(2, [], heap); // 3
+obj_o1_1.ref.push(obj_o2_2);
 const obj_o2 = VirtualObject.create(6, [obj_o2_1, obj_o2_2], heap); // 4
 
-const obj_o3 = VirtualObject.create(1, [obj, obj2], heap); // 5
+const obj_o3 = VirtualObject.create(1, [], heap); // 5
 
 const obj = VirtualObject.create(2, [obj_o1, obj_o2, obj_o3], heap); // 6
 const obj2 = VirtualObject.create(4, [obj_o1, obj], heap); // 7
+obj_o3.ref.push(obj);
+obj_o3.ref.push(obj2);
+
 heap.addReference(obj);
 heap.addReference(obj2);
 
